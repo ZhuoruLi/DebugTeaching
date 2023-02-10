5320c535ef42cbb7317926b4ef968c7682cf6df6
ZhuoruLi <douglashu0822@163.com>
2023-02-10 00:22:47 -0600

    try to use onDisREcieveMessage and acquireVsCodeApi

--- src/extension.ts
+++ src/extension.ts
@@ -1,81 +1,60 @@
 import * as vscode from 'vscode';
 
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "input-display-webview" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "debuglearning" is now active!');
+    let disposable = vscode.commands.registerCommand('input-display-webview.inputData', () => {
+        const panel = vscode.window.createWebviewPanel(
+            'inputDisplayWebview',
+            'Input Display Webview',
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true
+            }
+        );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('debuglearning.addbutton', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const panel = vscode.window.createWebviewPanel(
-			'interactive button',
-			'Track window',
-			vscode.ViewColumn.One,
-			{enableScripts: true}
-		);
-		panel.webview.html = getWebviewContent();
-	});
+        panel.webview.html = getWebviewContent();
 
-	context.subscriptions.push(disposable);
+        panel.webview.onDidReceiveMessage(
+            message => {
+				console.log(message);
+                switch (message.command) {
+
+                    case 'showInput':
+						console.log(message);
+                        vscode.window.showInformationMessage(`You entered: ${message.text}`);
+                        break;
+                }
+            },
+            undefined,
+            context.subscriptions
+        );
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
-
 function getWebviewContent() {
-	return `<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Teaching Debugging</title>
-	</head>
-	<body>
-        <form>
-            <div class="formBox">
-                <label for="title">Your question</label>
-                <input type="text" placeholder="Your Question" name="question" id="questionInput">
-            </div>
-            <div class="formBox">
-                <button id="btn">Click to Add</button>
-            </div>
-            <div id="msg">
-                <pre></pre>
-            </div>
-        </form>
-	
-		<script>
-            let questions = [];
-            const addQuestion = (event) => {
-                event.preventDefault();
-                let question = {
-                    description: document.getElementById('questionInput').value,
-                }
-                questions.push(question);
-                document.forms[0].reset(); // to clear the form for the next entries
-                //document.querySelector('form').reset();
-
-                //for display purposes only
-                console.warn('added' , {questions} );
-                let pre = document.querySelector('#msg pre');
-                pre.textContent = '\n' + JSON.stringify(questions, '\t', 2);
-
-                //saving to localStorage
-                localStorage.setItem('MyQuestionList', JSON.stringify(questions) );
-            }
-            document.addEventListener('DOMContentLoaded', ()=>{
-                document.getElementById('btn').addEventListener('click', addQuestion);
+    return `<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Input Display Webview</title>
+    </head>
+    <body>
+        <input type="text" id="inputBox">
+        <button id="submitButton">Submit</button>
+        <script>
+            const vscode = acquireVsCodeApi();
+            document.getElementById("text").addEventListener("click", () => {
+                const inputBox = document.getElementById("inputBox") as HTMLInputElement;
+                vscode.postMessage({
+                    command: 'showInput',
+                    text: inputBox.value
+                });
             });
-		</script>
-	</body>
-</html>
-	`;
+        </script>
+    </body>
+    </html>`;
 }
